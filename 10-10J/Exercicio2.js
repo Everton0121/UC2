931fe659445c97bf6d145a8cd4173f9a5be82cdb
Everton0121 <everton.edu.2009@gmail.com>
2024-11-12 19:53:15 -0300

    Delete 10-10J directory

--- 10-10J/Exercicio2.js
+++
@@ -1,9 +0,0 @@
-const number1 = 290
-
-if(number1 === 0 ){
-    console.log("Ele é 0")
-}else if(number1 > 0 ){
-    console.log("Ele é numero positivo")
-}else{
-    console.log("Ele é um numero negativo")
-}
--- 10-10J/Readline-Sync.js
+++
@@ -1,4 +0,0 @@
-const readline = require(`readline-sync`)
-const nome = readline.question("Qual seu nome ?")
-const sobreNome = readline.question("Qual seu sobrenome ?")
-console.log(`Olá, ${nome} ${sobreNome}.Tudo bem com você?`)
