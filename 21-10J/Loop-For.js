232a1d7493c959f8767ddb00503cfce81540a962
Everton0121 <everton.edu.2009@gmail.com>
2024-11-12 19:52:52 -0300

    Delete 21-10J directory

--- 21-10J/Loop-For.js
+++
@@ -1,5 +0,0 @@
-for (let numero = 10; numero >= 0; numero++) 
-    console.log(numero)
-    if(numero === 0){
-        console.log("Ele é o numero zer")
-}
--- 21-10J/exercicio1.js
+++
@@ -1,7 +0,0 @@
-let numero = 1
-
-while (numero <= 10) {
-    console.log (numero);
-    numero ++;
-}
-
